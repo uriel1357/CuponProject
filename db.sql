6b98e1915dd73b9126b5bdd453908126f566d6ab
yschrager <hezi_schrager@intuit.com>
2020-04-25 23:02:16 +0300

    add

---
+++ db.sql
@@ -0,0 +1,5 @@
+create table COMPANIES(id integer, email varchar(100), password varchar(100), name varchar(100));
+create table CUSTOMERS(id integer, email varchar(100), password varchar(100), firsr_name varchar(100), last_name varchar(100));
+create table CATEGORIES(id integer, name varchar(100));
+create table CUPONS(id integer, company_id integer, category_id integer, title varchar(100), description varchar(100), start_date Date, end_date Date,amount integer,  price double, image varchar(100));
+create table CUSTOMERS_VS_CUPONS(customer_id integer, cupon_id integer);
