cbc39ad0bf3188f6b567cb128e511be609a2212e
yschrager <hezi_schrager@intuit.com>
2020-04-26 16:18:12 +0300

    add

--- db.sql
+++ db.sql
@@ -3,3 +3,10 @@ create table CUSTOMERS(id integer, email varchar(100), password varchar(100), fi
 create table CATEGORIES(id integer, name varchar(100));
 create table CUPONS(id integer, company_id integer, category_id integer, title varchar(100), description varchar(100), start_date Date, end_date Date,amount integer,  price double, image varchar(100));
 create table CUSTOMERS_VS_CUPONS(customer_id integer, cupon_id integer);
+insert into COMPANIES (id , email , password , name ) values (1, "email@gmail.com", "pass", "comp");
+select count(*) from COMPANIES where  email="email@gmail.com" and password="pass";
+
+-- insert into Test(id, title) values(1, "Hello");
+-- select * from Test;
+-- Your code here!
+
